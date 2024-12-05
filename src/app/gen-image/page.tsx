df43b43ec1bd3fddd5ca217634ea3b75ec835c08
Van Dao <daovo739@gmail.com>
2024-12-05 16:46:27 +0700

    imgae

--- src/app/gen-image/page.tsx
+++ src/app/gen-image/page.tsx
@@ -3,24 +3,7 @@
 import { generateFromImage } from '@/service/gemini/service';
 import { useState } from 'react';
 
-const toBase64 = (file: File) => {
-  return new Promise((resolve, reject) => {
-    const fileReader = new FileReader();
-
-    fileReader.readAsDataURL(file);
-
-    fileReader.onload = () => {
-      resolve(fileReader.result);
-    };
-
-    fileReader.onerror = (error) => {
-      reject(error);
-    };
-  });
-};
-
 export default function Page() {
-  const [base64, setBase64] = useState<string | null>(null);
   const [file, setFile] = useState<File | null>(null);
 
   return (
@@ -37,15 +20,13 @@ export default function Page() {
             return;
           }
           setFile(file);
-          const base64 = (await toBase64(file)) as string;
-          setBase64(base64);
         }}
       />
 
       <button
         onClick={async () => {
-          if (!base64) return;
-          const rs = await generateFromImage(base64, file);
+          if (!file) return;
+          const rs = await generateFromImage(file);
           console.log(rs);
         }}
       >
