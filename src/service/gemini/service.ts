35acf574343d23df67342b8a1c702ff62caf043a
Van Dao <daovo739@gmail.com>
2024-12-06 11:49:01 +0700

    update

--- src/service/gemini/service.ts
+++ src/service/gemini/service.ts
@@ -1,161 +1,173 @@
-import {ChatSession, GoogleGenerativeAI, HarmBlockThreshold, HarmCategory,} from '@google/generative-ai';
-import {collection, doc, getDocs, orderBy, query, setDoc,} from 'firebase/firestore';
-import {db} from '@/utils/firebaseConfig';
-import {CollectDataForm} from '@/app/w/CollectData';
+import {
+  ChatSession,
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} from '@google/generative-ai';
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+} from 'firebase/firestore';
+import { db } from '@/utils/firebaseConfig';
+import { CollectDataForm } from '@/app/w/CollectData';
 
 const GENERATION_CONFIG = {
-    temperature: 0.9,
-    topK: 1,
-    topP: 1,
-    maxOutputTokens: 2048,
+  temperature: 0.9,
+  topK: 1,
+  topP: 1,
+  maxOutputTokens: 2048,
 };
 const SAFETY_SETTINGS = [
-    {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
 ];
 
 export async function initializeChatbot(): Promise<
-    | { success: true; chatSession: ChatSession }
-    | {
-    success: false;
-    error: string;
-}
-> {
-    try {
-        const genAI = new GoogleGenerativeAI(
-            process.env.NEXT_PUBLIC_GEMINI_API_KEY ?? ''
-        );
-        const model = genAI.getGenerativeModel({
-            model: process.env.NEXT_PUBLIC_MODEL_NAME ?? '',
-        });
-
-        const chatSession: ChatSession = model.startChat({
-            generationConfig: GENERATION_CONFIG,
-            safetySettings: SAFETY_SETTINGS,
-            history: [],
-        });
-
-        return {success: true, chatSession};
-    } catch (error) {
-        return {success: false, error: (error as Error).message};
+  | { success: true; chatSession: ChatSession }
+  | {
+      success: false;
+      error: string;
     }
+> {
+  try {
+    const genAI = new GoogleGenerativeAI(
+      process.env.NEXT_PUBLIC_GEMINI_API_KEY ?? ''
+    );
+    const model = genAI.getGenerativeModel({
+      model: process.env.NEXT_PUBLIC_MODEL_NAME ?? '',
+    });
+
+    const chatSession: ChatSession = model.startChat({
+      generationConfig: GENERATION_CONFIG,
+      safetySettings: SAFETY_SETTINGS,
+      history: [],
+    });
+
+    return { success: true, chatSession };
+  } catch (error) {
+    return { success: false, error: (error as Error).message };
+  }
 }
 
 export async function handleUserInput(input: {
-    dataForm?: CollectDataForm;
-    userInput: string;
-    chatSession?: ChatSession;
+  dataForm?: CollectDataForm;
+  userInput: string;
+  chatSession?: ChatSession;
 }): Promise<
-    | {
-    success: true;
-    response: string;
-}
-    | { success: false; error: string }
+  | {
+      success: true;
+      response: string;
+    }
+  | { success: false; error: string }
 > {
-    const {dataForm, userInput, chatSession} = input;
-    let newChatSession: ChatSession | undefined = chatSession;
+  const { dataForm, userInput, chatSession } = input;
+  let newChatSession: ChatSession | undefined = chatSession;
+  if (!newChatSession) {
+    const genAI = new GoogleGenerativeAI(
+      process.env.NEXT_PUBLIC_GEMINI_API_KEY ?? ''
+    );
+    const model = genAI.getGenerativeModel({
+      model: process.env.NEXT_PUBLIC_MODEL_NAME ?? '',
+    });
+
+    newChatSession = model.startChat({
+      generationConfig: GENERATION_CONFIG,
+      safetySettings: SAFETY_SETTINGS,
+      history: [],
+    });
+  }
+
+  try {
     if (!newChatSession) {
-        const genAI = new GoogleGenerativeAI(
-            process.env.NEXT_PUBLIC_GEMINI_API_KEY ?? ''
-        );
-        const model = genAI.getGenerativeModel({
-            model: process.env.NEXT_PUBLIC_MODEL_NAME ?? '',
-        });
-
-        newChatSession = model.startChat({
-            generationConfig: GENERATION_CONFIG,
-            safetySettings: SAFETY_SETTINGS,
-            history: [],
-        });
+      return {
+        success: false,
+        error: 'Chatbot not initialized. Please refresh the page.',
+      };
     }
-
-    try {
-        if (!newChatSession) {
-            return {
-                success: false,
-                error: 'Chatbot not initialized. Please refresh the page.',
-            };
-        }
-        const result = await newChatSession.sendMessage(userInput);
-        if (dataForm) await saveChatLog(dataForm, result.response.text());
-        return {success: true, response: result.response.text()};
-    } catch (error) {
-        return {success: false, error: (error as Error).message};
-    }
+    const result = await newChatSession.sendMessage(userInput);
+    if (dataForm) await saveChatLog(dataForm, result.response.text());
+    return { success: true, response: result.response.text() };
+  } catch (error) {
+    return { success: false, error: (error as Error).message };
+  }
 }
 
 export async function saveChatLog(
-    dataForm: CollectDataForm,
-    botResponse: string
+  dataForm: CollectDataForm,
+  botResponse: string
 ): Promise<void> {
-    try {
-        const chatLogRef = doc(db, 'chatLogs', Date.now().toString());
-        await setDoc(chatLogRef, {
-            dataForm,
-            botResponse,
-            timestamp: new Date().toISOString(),
-        });
-        console.log('Chat log saved successfully!');
-    } catch (error) {
-        console.error('Error saving chat log:', error);
-    }
+  try {
+    const chatLogRef = doc(db, 'chatLogs', Date.now().toString());
+    await setDoc(chatLogRef, {
+      dataForm,
+      botResponse,
+      timestamp: new Date().toISOString(),
+    });
+    console.log('Chat log saved successfully!');
+  } catch (error) {
+    console.error('Error saving chat log:', error);
+  }
 }
 
 export async function getChatLogs() {
-    try {
-        const chatLogsRef = collection(db, 'chatLogs');
+  try {
+    const chatLogsRef = collection(db, 'chatLogs');
 
-        const queryBuilder = query(chatLogsRef, orderBy('timestamp', 'desc'));
+    const queryBuilder = query(chatLogsRef, orderBy('timestamp', 'desc'));
 
-        const querySnapshot = await getDocs(queryBuilder);
+    const querySnapshot = await getDocs(queryBuilder);
 
-        return querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...(doc.data() as {
-                dataForm: CollectDataForm;
-                botResponse: string;
-                timestamp: string;
-            }),
-        }));
-    } catch (error) {
-        console.error('Error fetching chat logs:', error);
-        throw new Error('Unable to fetch chat logs.');
-    }
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...(doc.data() as {
+        dataForm: CollectDataForm;
+        botResponse: string;
+        timestamp: string;
+      }),
+    }));
+  } catch (error) {
+    console.error('Error fetching chat logs:', error);
+    throw new Error('Unable to fetch chat logs.');
+  }
 }
 
 export async function generateFromImage(file: File) {
-    try {
-        const genAI = new GoogleGenerativeAI(
-            process.env.NEXT_PUBLIC_GEMINI_API_KEY ?? ''
-        );
-        const model = genAI.getGenerativeModel({
-            model: process.env.NEXT_PUBLIC_MODEL_NAME ?? '',
-        });
+  try {
+    const genAI = new GoogleGenerativeAI(
+      process.env.NEXT_PUBLIC_GEMINI_API_KEY ?? ''
+    );
+    const model = genAI.getGenerativeModel({
+      model: process.env.NEXT_PUBLIC_MODEL_NAME ?? '',
+    });
 
-        const rs = await file.arrayBuffer();
+    const rs = await file.arrayBuffer();
 
-        const result = await model.generateContent([
-            {
-                inlineData: {
-                    data: Buffer.from(rs).toString('base64'),
-                    mimeType: 'image/jpeg',
-                },
-            },
-            `
+    const result = await model.generateContent([
+      {
+        inlineData: {
+          data: Buffer.from(rs).toString('base64'),
+          mimeType: 'image/jpeg',
+        },
+      },
+      `
       Hãy phân tích hình ảnh được tải lên để xác định các món ăn xuất hiện. Sau đó, hãy cung cấp thông tin chi tiết theo định dạng sau:
 
       **1. [Tên món ăn 1]**
@@ -168,13 +180,13 @@ export async function generateFromImage(file: File) {
       **2. [Tên món ăn 1]**
       
 
-      Hãy cung cấp thông tin chi tiết và rõ ràng theo đúng định dạng trên. Với mỗi món ăn sẽ là một section
+      Hãy cung cấp thông tin chi tiết và rõ ràng theo đúng định dạng trên. Với mỗi món ăn sẽ là một section, section bắt đầu với **[Tên món ăn]**, mỗi ý sẽ bắt đầu với dấu "-".
       Nếu hình ảnh không có bất kỳ món ăn nào, hãy trả về đoạn text ngắn gọn nhất trong đó có từ "không hợp lệ"
       `.trim(),
-        ]);
+    ]);
 
-        return result.response.text();
-    } catch (error) {
-        console.log(error);
-    }
+    return result.response.text();
+  } catch (error) {
+    console.log(error);
+  }
 }
