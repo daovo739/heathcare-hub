9e6fa6de753696b0b3ecb9d6f072840f79c7356d
Van Dao <daovo739@gmail.com>
2024-12-06 13:41:08 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,8 @@
-import { Inter } from 'next/font/google';
+import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { Provider } from './Provider';
-import { Toaster } from '@/components/ui/toaster';
-
-const dinRoundSans = localFont({
-  src: [
-    { path: './fonts/dinroundpro.otf', weight: '400', style: 'normal' },
-    {
-      path: './fonts/dinroundpro_black.otf',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: './fonts/dinroundpro_bold.otf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './fonts/dinroundpro_light.otf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './fonts/dinroundpro_medi.otf',
-      weight: '500',
-      style: 'normal',
-    },
-  ],
-});
 
 export const metadata: Metadata = {
   title: 'Enouvo Healthcare Hub',
