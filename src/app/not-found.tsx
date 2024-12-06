d4073c6054affe10fe86b8f49c194af160904787
Van Dao <daovo739@gmail.com>
2024-12-06 10:28:16 +0700

    update

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-3 items-center">
+        <h1 className="text-2xl font-bold">
+          Tính năng đang trong quá trình phát triển, hãy quay lại sau ...
+        </h1>
+        <Button onClick={() => router.push('/')}>Trở về trang chủ</Button>
+      </div>
+    </div>
+  );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import { cn } from '@/lib/utils'; // Ensure you have this from shadcn
-import { History, Home, Settings } from 'lucide-react'; // Icons from lucide-react
+import {
+  Dices,
+  Gamepad,
+  History,
+  Home,
+  PersonStanding,
+  Settings,
+} from 'lucide-react'; // Icons from lucide-react
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -10,13 +17,26 @@ import emPi from '@/assets/images/emPi.png';
 const menuItems = [
   { name: 'Trang chủ', icon: Home, href: '/dashboard' },
   { name: 'Lịch sử tư vấn', icon: History, href: '/history' },
-  { name: 'Cài đặt', icon: Settings, href: '/settings' },
+  { name: 'Cài đặt', icon: Settings, href: '/settings', comingSoon: true },
+  {
+    name: 'Đề xuất chương trình luyện tập',
+    icon: Dices,
+    href: '/suggest',
+    comingSoon: true,
+  },
+  {
+    name: 'Đề xuất hỗ trợ tinh thần',
+    icon: PersonStanding,
+    href: '/support',
+    comingSoon: true,
+  },
+  { name: 'Trò chơi', icon: Gamepad, href: '/game', comingSoon: true },
 ];
 export function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <div className="w-64 bg-white text-neutral-900 flex flex-col pt-8">
+    <div className="max-w-[24rem] min-w-64 bg-white text-neutral-900 flex flex-col pt-8">
       {/* Logo */}
       <div className="h-16 flex items-center justify-center text-2xl font-bold">
         <div className="relative size-36">
@@ -32,14 +52,22 @@ export function Sidebar() {
             key={item.name}
             href={item.href}
             className={cn(
-              'flex items-center gap-4 p-2 rounded transition text-neutral-600 hover:bg-primary hover:text-primary-foreground mb-3',
+              'flex flex-wrap gap-4 p-2 rounded-md transition text-neutral-600 hover:bg-primary hover:text-primary-foreground mb-3',
               {
                 'bg-primary text-primary-foreground': pathname === item.href,
+                'pointer-events-none text-neutral-400': item.comingSoon,
               }
             )}
           >
-            <item.icon className="size-5" />
-            <span>{item.name}</span>
+            <div className="flex items-center gap-4">
+              <item.icon className="size-5" />
+              <span>{item.name}</span>
+            </div>
+            {item.comingSoon && (
+              <span className="text-xs border solid border-primary text-primary px-3 py-2 rounded-full w-fit p-2">
+                Sắp ra mắt ...
+              </span>
+            )}
           </Link>
         ))}
       </nav>
